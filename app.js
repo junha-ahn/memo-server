de0584201d619b185c412133dfa6d1244a8dd0ff
Junha Ahn <junha.ahn.dev@gmail.com>
2020-01-06 22:45:43 +0900

    mongo db connect

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const compression = require('compression');
-
+const cors = require("cors");
+const connect = require('./config/mongo');
 const routes = $require("routes");
 
 app.use(helmet());
 app.use(helmet.noCache());
 app.use(compression());
+app.use(cors());
 
 app.use(cookieParser(process.env.COOKE_SECRET));
 app.use(morgan(process.env.NODE_ENV == 'production' ? 'combined' : 'dev'));
@@ -22,5 +24,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 routes(app);
+
+connect();
 
 module.exports = app;
