9f924bece1a35ad2cda5e2548e4f96fdb8055173
junha-ahn <junha.ahn.dev@gmail.com>
2020-02-07 16:34:53 +0900

    config 관련 수정

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -16,27 +16,27 @@ module.exports = app => {
   app.use(helmet.noCache());
   app.use(cors());
 
-  app.use(cookieParser(process.env.COOKE_SECRET));
+  app.use(cookieParser(config.COOKE_SECRET));
   app.use(
     session({
-      secret: process.env.COOKE_SECRET,
+      secret: config.COOKE_SECRET,
       saveUninitialized: false,
       resave: false,
       cookie: {
         httpOnly: true,
         secure: false
       },
-      store: process.env.REDIS_ENABLE === 1 ?
+      store: config.redis.enable === 1 ?
         store : new RedisStore({
           client: redisClient,
-          host: process.env.REDIS_HOST,
-          port: process.env.REDIS_PORT,
+          host: config.redis.host,
+          port: config.redis.port,
           logErrors: true,
           ttl: 60 * 60 * 24 // in seconds (redis will delete automatically)
         })
     })
   );
-  app.use(morgan(process.env.NODE_ENV == "production" ? "combined" : "dev"));
+  app.use(morgan(config.NODE_ENV == "production" ? "combined" : "dev"));
 
   app.use(
     bodyParser.urlencoded({
--- src/loaders/logger.js
+++ src/loaders/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const config = $require('config');
 
 const transports = [];
-if (process.env.NODE_ENV !== 'development') {
+if (config.NODE_ENV !== 'development') {
   transports.push(
     new winston.transports.Console()
   )
