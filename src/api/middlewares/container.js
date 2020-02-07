0469651992e8bd77c100381b115965882b5138b3
junha-ahn <junha.ahn.dev@gmail.com>
2020-02-07 17:08:36 +0900

    container error log 수정

--- src/api/middlewares/container.js
+++ src/api/middlewares/container.js
@@ -1,7 +1,9 @@
+const config = $require('config')
 const {
   sendResponse,
   fail
 } = require('./response');
+
 
 module.exports = service => async (req, res) => {
   const response = sendResponse(res);
@@ -11,13 +13,10 @@ module.exports = service => async (req, res) => {
       errorMessage: '반환값이 명시되지 않았습니다.'
     }));
   } catch (e) {
-    console.error(e);
-    if (e.response) {
-      const error = e.response;
-      const {
-        httpCode = 500, data = {}
-      } = error;
-      res.status(httpCode).json(data);
+    if (config.NODE_ENV === 'development') console.error(e)
+    if (e.status !== undefined) {
+      const httpCode = e.httpCode || 500;
+      return res.status(httpCode).json(e);
     }
     response(fail.error.internalError({
       errorMessage: e.message || ''
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Auth', () => {
-  beforeEach((done) => { //Before each test we empty the database
+  before((done) => { //Before each test we empty the database
     User.deleteMany({}, (err) => {
       done();
     });
@@ -18,7 +18,7 @@ describe('Auth', () => {
         chai.request(server)
           .post('/api/auth/signup')
           .send({
-            email: 'email@eamil.com',
+            email: 'email@email.com',
             password: '1234',
             name: '테스트'
           })
@@ -51,7 +51,7 @@ describe('Auth', () => {
         chai.request(server)
           .post('/api/auth/signup')
           .send({
-            email: 'email@eamil.com',
+            email: 'email@email.com',
             password: '1234',
             name: '테스트'
           })
@@ -63,66 +63,66 @@ describe('Auth', () => {
       });
     })
   })
-  // describe('/POST signin은', () => {
-  //   describe('성공시', () => {
-  //     it('토큰을 반환한다', done => {
-  //       chai.request(server)
-  //         .post('/api/auth/signin')
-  //         .send({
-  //           eamil: 'email@email.com',
-  //           password: '1234',
-  //         })
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(200);
-  //           expect(res.body.status).to.deep.equal(true);
-  //           expect(res.body.data).to.be.an('object');
-  //           expect(res.body.data).to.have.property('token');
-  //           expect(res.body.data).to.have.property('info');
-  //           expect(res.body.data.info).to.be.an('object');
-  //           done();
-  //         });
-  //     });
-  //   })
-  //   describe('실패시', () => {
-  //     it('비밀번호를 잘못 입력했을때 401을 반환한다', done => {
-  //       chai.request(server)
-  //         .post('/api/auth/signin')
-  //         .send({
-  //           eamil: 'email@email.com',
-  //           password: '4321',
-  //         })
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(401);
-  //           expect(res.body.status).to.deep.equal(false);
-  //           done();
-  //         });
-  //     });
+  describe('/POST signin은', () => {
+    describe('성공시', () => {
+      it('토큰을 반환한다', done => {
+        chai.request(server)
+          .post('/api/auth/signin')
+          .send({
+            email: 'email@email.com',
+            password: '1234',
+          })
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res.body.status).to.deep.equal(true);
+            expect(res.body.data).to.be.an('object');
+            expect(res.body.data).to.have.property('token');
+            expect(res.body.data).to.have.property('info');
+            expect(res.body.data.info).to.be.an('object');
+            done();
+          });
+      });
+    })
+    describe('실패시', () => {
+      it('비밀번호를 잘못 입력했을때 400을 반환한다', done => {
+        chai.request(server)
+          .post('/api/auth/signin')
+          .send({
+            email: 'email@email.com',
+            password: '4321',
+          })
+          .end((err, res) => {
+            expect(res).to.have.status(400);
+            expect(res.body.status).to.deep.equal(false);
+            done();
+          });
+      });
 
-  //   })
-  // })
-  // describe('/GET exists/email은', () => {
-  //   describe('성공시', () => {
-  //     it('이미 존재하는 이메일을 입력했을때 200반환', done => {
-  //       chai.request(server)
-  //         .get('/api/auth/exists/email/email@email.com')
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(200);
-  //           expect(res.body.status).to.deep.equal(true);
-  //           done();
-  //         });
-  //     });
-  //   })
-  //   describe('실패시', () => {
-  //     it('사용 가능한 이메일 입력시 404 반환', done => {
-  //       chai.request(server)
-  //         .get('/api/auth/exists/email/available@email.com')
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(404);
-  //           expect(res.body.status).to.deep.equal(false);
-  //           done();
-  //         });
-  //     });
+    })
+  })
+  describe('/GET exists/email은', () => {
+    describe('성공시', () => {
+      it('이미 존재하는 이메일을 입력했을때 200반환', done => {
+        chai.request(server)
+          .get('/api/auth/exists/email/email@email.com')
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res.body.status).to.deep.equal(true);
+            done();
+          });
+      });
+    })
+    describe('실패시', () => {
+      it('사용 가능한 이메일 입력시 404 반환', done => {
+        chai.request(server)
+          .get('/api/auth/exists/email/available@email.com')
+          .end((err, res) => {
+            expect(res).to.have.status(404);
+            expect(res.body.status).to.deep.equal(false);
+            done();
+          });
+      });
 
-  //   })
-  // })
+    })
+  })
 });
